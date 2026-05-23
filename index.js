3d4e70e2c8c87b70ff211840b1c89e46b9544c5c
jamalitechempire <jamalitechtz1@gmail.com>
2026-05-24 01:08:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/login', (req, res) => {
 
 // Multiple users
 const ADMINS = [
-  { username: 'hansadewmina', password: 'hansa2008122@' },
+  { username: 'jamali1234', password: 'jamalitech1' },
 ];
 
 app.post('/api/login', (req, res) => {
